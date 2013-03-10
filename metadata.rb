db38fa3daee0e3de0d36c1fae9e8d1082f6a03be
Michael <hfeeki@gmail.com>
2013-03-10 09:22:31 +0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description       "Installs Python, pip and virtualenv. Includes LWRPs for manag
 version           "1.2.2"
 
 depends           "build-essential"
+#depends           "yum"
 
 recipe "python", "Installs python, pip, and virtualenv"
 recipe "python::package", "Installs python using packages."
