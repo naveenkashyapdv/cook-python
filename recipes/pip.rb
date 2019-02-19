27dff8272d46fe43e1d273e9712197937466fdc6
naveenkashyapdv <d.naveenkashyap@gmail.com>
2019-02-18 16:16:29 -0800

    Update pip.rb

--- recipes/pip.rb
+++ recipes/pip.rb
@@ -31,7 +31,7 @@ end
 # http://stackoverflow.com/questions/4324558/whats-the-proper-way-to-install-pip-virtualenv-and-distribute-for-python
 # https://bitbucket.org/ianb/pip/issue/104/pip-uninstall-on-ubuntu-linux
 remote_file "#{Chef::Config[:file_cache_path]}/distribute_setup.py" do
-  source "http://python-distribute.org/distribute_setup.py"
+  source "https://bootstrap.pypa.io/get-pip.py"
   mode "0644"
   not_if { ::File.exists?(pip_binary) }
 end
@@ -39,7 +39,7 @@ end
 execute "install-pip" do
   cwd Chef::Config[:file_cache_path]
   command <<-EOF
-  #{node['python']['binary']} distribute_setup.py
+  #{node['python']['binary']} get-pip.py.py
   #{::File.dirname(pip_binary)}/easy_install pip
   EOF
   not_if { ::File.exists?(pip_binary) }
